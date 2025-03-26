23b396edf6bdfc838fa400241de97f0b0addac43
vernonthedev <techjaja2@gmail.com>
2025-03-26 13:27:29 +0300

    #init: Initial Commit

---
+++ bin/dart_console.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console/dart_console.dart' as dart_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console.calculate()}!');
+}
---
+++ test/dart_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console/dart_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
